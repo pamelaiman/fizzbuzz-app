6d547fb21c0f69f0511b7abb82fc0688270cd584
Pamela Iman <pamelariman@gmail.com>
2024-06-17 14:09:17 +0100

    fizzbuzz

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,27 @@ import { generateFizzBuzz } from './generateFizzBuzz'
 
 
 export default function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
     
     function increase() {
       let newCount = count + 1
       setCount(newCount)
     }
     
-    function decrease() {
-      let newCount = count - 1
+    function reset() {
+      let newCount = 1
       setCount(newCount)
     }
     let checkedForFB = generateFizzBuzz(count)
     
     return (
         <div className="counter">
-            <button className="counter--minus" onClick={decrease}>–</button>
+            <button className="counter--minus" onClick={reset}>Reset</button>
             <div>
-                <h1>{checkedForFB}</h1>
+              <h1>{checkedForFB}</h1>
+                <h3> Click `Reset` to start again</h3>
             </div>
-            <button className="counter--plus" onClick={increase}>+</button>
+            <button className="counter--plus" onClick={increase}>Next</button>
   
         </div>
     )
