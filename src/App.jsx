678c7976713d3acdeb39528612488c802444bcc6
Pamela Iman <pamelariman@gmail.com>
2024-06-17 14:02:07 +0100

    fizzbuzz

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { generateFizzBuzz } from './generateFizzBuzz'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const [count, setCount] = useState(0)
+    
+    function increase() {
+      let newCount = count + 1
+      setCount(newCount)
+    }
+    
+    function decrease() {
+      let newCount = count - 1
+      setCount(newCount)
+    }
+    let checkedForFB = generateFizzBuzz(count)
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={decrease}>–</button>
+            <div>
+                <h1>{checkedForFB}</h1>
+            </div>
+            <button className="counter--plus" onClick={increase}>+</button>
+  
+        </div>
+    )
 }
-
-export default App
